3166b01ce7ae62195a2b5d15e26ac4a116e262ca
ibrus <brus.andreevich@gmail.com>
2019-02-25 17:10:11 +0200

    add

---
+++ lesson1/src/ua/od/hillel/FirstProgram.java
@@ -0,0 +1,8 @@
+package ua.od.hillel;
+
+public class FirstProgram {
+	
+	public static void main(String[] args){
+		System.out.println("My name is " + args[0] + " and this is my first java program");
+	}
+}
